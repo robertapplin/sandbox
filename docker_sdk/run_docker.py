4a9ca80d1a2ae53e691eaabf5b67bb70283f15f1
Robert <robert.applin@stfc.ac.uk>
2023-04-20 14:54:37 +0100

    Use a bind mount to persist results

--- docker_sdk/run_docker.py
+++ docker_sdk/run_docker.py
@@ -1,7 +1,10 @@
 import docker
+import os
 
 client = docker.from_env()
-container = client.containers.run("autotest", ["1", "5", "100"], detach=True)
+container = client.containers.run("autotest", ["1", "5", "100"],
+                                  volumes={os.path.join(os.getcwd(), "output"): {"bind": "/app/output/", "mode": "rw"}},
+                                  detach=True)
 
 print(container.id)
 print(container.logs())
