96763c57bc4bacc6fd95a9af2e051b2019236159
Robert Applin <robertapplin.developer@gmail.com>
2023-04-22 19:02:41 +0100

    Ability to add data to the database

--- compose_app/my_site/basic_app/urls.py
+++ compose_app/my_site/basic_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import IndexView
 
+app_name = 'basic_app'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", IndexView.as_view(), name="index"),
 ]
--- compose_app/my_site/basic_app/views.py
+++ compose_app/my_site/basic_app/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import redirect, render
+from django.views import generic
+
+from .models import Question
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the basic app index.")
+class IndexView(generic.TemplateView):
+    """
+    A view for the index page.
+    """
+
+    template_name = "basic_app/index.html"
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        """Handles a get request."""
+        context = {
+            "content": "This is some content",
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        question_text = request.POST["question"]
+
+        question = Question(question_text=question_text)
+        question.save()
+
+        return redirect("basic_app:index")
