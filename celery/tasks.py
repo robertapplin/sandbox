24215fb26c843cd783ac31c2af2dbd86a521e513
Robert Applin <robertapplin.developer@gmail.com>
2023-05-07 14:39:02 +0100

    Add example code for customizing a task

--- celery/tasks.py
+++ celery/tasks.py
@@ -8,12 +8,25 @@ app = Celery("tasks")
 app.config_from_object("celeryconfig")
 
 
+class SpecialTask(Task):
+
+    def on_success(self, retval, task_id, args, kwargs):
+        # args are the original args provided to the task
+        # kwargs are the original kwargs provided to the task
+        # retval is the task return value
+        pass
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        # Do a failure
+        print(self.name)  # Is the name of the task
+
+
 @app.task
 def add(x, y):
     return x + y
 
 
-@app.task
+@app.task(base=SpecialTask, name="Run a Qt Widget")
 def run_qt():
     app = QApplication(["app_name"])
     widget = UserWidget()
